1e84930a5fecb58c0bdb5e9fac96dbd1d14c08bc
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2022-07-25 15:34:28 +0900

    index view

--- comments/urls.py
+++ comments/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'comments'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.CommentIndexView.as_view(), name='index'),
 ]
