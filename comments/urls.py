6e7d2663f943d69042be35306968ceef2695f9f5
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2022-07-25 15:42:47 +0900

    detail

--- comments/urls.py
+++ comments/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'comments'
 urlpatterns = [
     path('', views.CommentIndexView.as_view(), name='index'),
+    path('<int:pk>', views.ShowCommentView.as_view(), name='show'),
 ]
--- comments/views.py
+++ comments/views.py
@@ -1,6 +1,7 @@
 # from django.shortcuts import render
 # from django.http import HttpResponse
 from django.views.generic import ListView
+from django.views.generic import DetailView
 from .models import Comment
 
 # Create your views here.
@@ -8,4 +9,7 @@ from .models import Comment
 #     return HttpResponse("Hello, world. You're at the comments index.")
 
 class CommentIndexView(ListView):
+  model = Comment
+
+class ShowCommentView(DetailView):
   model = Comment
